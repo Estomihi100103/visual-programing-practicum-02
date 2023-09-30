31b895243fab0226d48a5b2e6231d0253761bee2
GenesisPanjaitan <genesispanjaitan3@gmail.com>
2023-09-30 15:16:27 +0700

    12S23012 - Genesis Tombak Dapot Tua Panjaitan
    Saya telah menyelesaikan P02_01 dan P02_02

---
+++ P02_01.java
@@ -0,0 +1,40 @@
+import java.util.*;
+import java.lang.Math;
+
+class Program {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String program, kode, matkul, dosen, tanggal, format, status, status2;
+        int tingkatKesulitan, hariDeadline;
+        double prioritas;
+
+        program = input.nextLine();
+        kode = input.nextLine();
+        matkul = input.nextLine();
+        dosen = input.nextLine();
+        tanggal = input.nextLine();
+        format = input.nextLine();
+        tingkatKesulitan = input.nextInt();
+        hariDeadline = input.nextInt();
+        status = input.nextLine();
+        prioritas = tingkatKesulitan * (1.0 / hariDeadline);
+        System.out.println("Prioritas: " + toFixed(prioritas,2));
+        if (prioritas > 3) {
+            status2 = "Penting! Anda harus mengerjakan tugas ini segera.";
+        } else {
+            if (prioritas <= 3 && prioritas >= 1.5) {
+                status2 = "Tugas ini memiliki prioritas menengah.";
+            } else {
+                if (prioritas < 1.5) {
+                    status2 = "Tugas ini relatif ringan, namun jangan tunda terlalu lama.";
+                }
+            }
+        }
+        System.out.println(program + "|" + kode + "|" + matkul + "|" + dosen + "|" + tanggal + "|" + format + "|" + status + "|" + status2);
+    }
+    
+    private static String toFixed(double value, int digits) {
+        return String.format("%." + digits + "f", value);
+    }
+}
---
+++ P02_02.java
@@ -0,0 +1,32 @@
+// 12S23012 - Genesis Tombak Dapot Tua Panjaitan
+import java.util.*;
+import java.lang.Math;
+
+class P02 {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String nama, ac, ketersediaan, teknologi, lantai, skor2;
+        int kapasitas, jumlah, skor;
+
+        nama = input.nextLine();
+        kapasitas = input.nextInt();
+        jumlah = input.nextInt();
+        ac = input.nextLine();
+        lantai = input.nextLine();
+        ketersediaan = input.nextLine();
+        skor = (int) ((double) jumlah / kapasitas * 100);
+        if (skor >= 40) {
+            skor2 = "Gudang Elite";
+        } else {
+            if (skor <= 40 && skor >= 25) {
+                skor2 = "Gudang Standar";
+            } else {
+                if (skor < 25) {
+                    skor2 = "Gudang Perlu Peningkatan";
+                }
+            }
+        }
+        System.out.println("Informasi Gudang : " + nama + "|" + kapasitas + "|" + jumlah + "|" + ac + "|" + lantai + "|" + ketersediaan + "|" + skor + "|" + skor2);
+    }
+}
