1f856a5594bed46e06b22109479aeb4feb90aebb
GenesisPanjaitan <genesispanjaitan3@gmail.com>
2023-09-30 17:56:41 +0700

    Update P02_01.java

--- P02_01.java
+++ P02_01.java
@@ -1,7 +1,7 @@
 import java.util.*;
 import java.lang.Math;
 
-class Program {
+class T01 {
     private static Scanner input = new Scanner(System.in);
 
     public static void main(String[] args) {
