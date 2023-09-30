143f14ea8bc7463df55d4d74bf705dd6f71c604e
tiffanibutarbutar <tiffanibutarbutar08@gmail.com>
2023-09-30 21:03:02 +0700

    sudah memperbaiki kesalahan

--- P02_01.java
+++ P02_01.java
@@ -2,7 +2,7 @@
 import java.util.*;
 import java.lang.Math;
 
-public class P02_01 {
+public class JavaApplication {
     private static Scanner input = new Scanner(System.in);
 
     public static void main(String[] args) {
--- P02_02.java
+++ P02_02.java
@@ -2,40 +2,31 @@
 import java.util.*;
 import java.lang.Math;
 
-public class P02_02 {
+public class JavaApplication {
     private static Scanner input = new Scanner(System.in);
 
     public static void main(String[] args) {
-        String deskripsiTugas, kodeMataKuliah, namaMataKuliah, pengampu, deadline, formatPenamaanTugas, status;
-        double tingkatKesulitan, hariHinggaDeadline, prioritas;
-        String rekomendasi;
+        String namaGudang, aC, lantai, ketersediaanTeknologi, penilaian;
+        int kapasitasGudang, jumlahBuku, skor;
 
-        deskripsiTugas = input.nextLine();
-        kodeMataKuliah = input.nextLine();
-        namaMataKuliah = input.nextLine();
-        pengampu = input.nextLine();
-        deadline = input.nextLine();
-        formatPenamaanTugas = input.nextLine();
-        tingkatKesulitan = Double.parseDouble(input.nextLine());
-        hariHinggaDeadline = Double.parseDouble(input.nextLine());
-        status = input.nextLine();
-        prioritas = tingkatKesulitan * (1.0 / hariHinggaDeadline);
-        if (prioritas > 3) {
-            rekomendasi = "Penting! Anda harus mengerjakan tugas ini segera.";
+        namaGudang = input.nextLine();
+        kapasitasGudang = Integer.parseInt(input.nextLine());
+        jumlahBuku = Integer.parseInt(input.nextLine());
+        aC = input.nextLine();
+        lantai = input.nextLine();
+        ketersediaanTeknologi = input.nextLine();
+        skor = (double) jumlahBuku / kapasitasGudang * 100;
+        if (skor >= 40) {
+            penilaian = "Gudang Elite";
         } else {
-            if (prioritas >= 1.5 && prioritas <= 3) {
-                rekomendasi = "Tugas ini memiliki prioritas menengah.";
+            if (skor >= 25 && skor < 40) {
+                penilaian = "Gudang Standar";
             } else {
-                if (prioritas < 1.5) {
-                    rekomendasi = "Tugas ini relatif ringan, namun jangan tunda terlalu lama.";
+                if (skor < 25) {
+                    penilaian = "Gudang Perlu Peningkatan";
                 }
             }
         }
-        System.out.println("Prioritas: " + toFixed(prioritas,2));
-        System.out.println(deskripsiTugas + "|" + kodeMataKuliah + "|" + namaMataKuliah + "|" + pengampu + "|" + deadline + "|" + formatPenamaanTugas + "|" + status + "|" + rekomendasi);
-    }
-    
-    private static String toFixed(double value, int digits) {
-        return String.format("%." + digits + "f", value);
+        System.out.println("Informasi Gudang: " + namaGudang + "|" + kapasitasGudang + "|" + jumlahBuku + "|" + aC + "|" + lantai + "|" + ketersediaanTeknologi + "|" + skor + "|" + penilaian);
     }
 }
