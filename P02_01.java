bfa998d930179ff70f0d3e96dc82c91ddb066846
AriellaSihombing <ellasihombing12@gmail.com>
2023-09-30 20:24:23 +0700

    sudah menyelesaikan P02

--- P02_01.java
+++ P02_01.java
@@ -1,1 +1,66 @@
-// NIM - Your Name
+// 12S23005 - Ariella U.C Sihombing
+import java.util.*;
+import java.lang.Math;
+
+class P02_01 {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String namaKonser;
+        String tanggalKonser;
+        String jenisTiket;
+        int nomorTiket;
+        int hargaTiket;
+        int jumlahTiket;
+
+        System.out.println("1. VIP Rp : 300000, sisa 20 tiket");
+        System.out.println("2. Reguler Rp : 200000, sisa 50 tiket");
+        System.out.println("3. Economy Rp : 100000, sisa 10 tiket");
+        System.out.println("NamaKonser");
+        System.out.println("Tanggal Konser");
+        System.out.println("Jenis Tiket");
+        System.out.println("Nomor Tiket");
+        System.out.println("Jumlah Tiket");
+        namaKonser = input.nextLine();
+        tanggalKonser = input.nextLine();
+        jenisTiket = input.nextLine();
+        nomorTiket = input.nextInt();
+        jumlahTiket = input.nextInt();
+        if (jenisTiket.equals("VIP")) {
+            if (jumlahTiket > 20) {
+                System.out.println("JumlahTiketTidakTersedia");
+            } else {
+                if (jumlahTiket > 3 && jumlahTiket <= 20) {
+                    hargaTiket = (int) (jumlahTiket * 300000 - 300000 * ((double) 5 / 100));
+                } else {
+                    hargaTiket = jumlahTiket * 300000;
+                }
+            }
+        } else {
+            if (jenisTiket.equals("Reguler")) {
+                if (jumlahTiket > 50) {
+                    System.out.println("JumlahTiketTidakTersedia");
+                } else {
+                    if (jumlahTiket > 3 && jumlahTiket <= 50) {
+                        hargaTiket = (int) (jumlahTiket * 200000 - 200000 * ((double) 5 / 100));
+                    } else {
+                        hargaTiket = jumlahTiket * 200000;
+                    }
+                }
+            } else {
+                if (jenisTiket.equals("Economy")) {
+                    if (jumlahTiket > 10) {
+                        System.out.println("JumlahTiketTidakTersedia");
+                    } else {
+                        if (jumlahTiket > 3 && jumlahTiket <= 10) {
+                            hargaTiket = (int) (jumlahTiket * 100000 - 100000 * ((double) 5 / 100));
+                        } else {
+                            hargaTiket = jumlahTiket * 100000;
+                        }
+                    }
+                }
+            }
+        }
+        System.out.println(namaKonser + "|" + tanggalKonser + "|" + jenisTiket + "|" + nomorTiket + "|" + jumlahTiket + "|" + hargaTiket);
+    }
+}
--- P02_02.java
+++ P02_02.java
@@ -1,1 +1,39 @@
-//NIM -Nama
+//12S23005 - Ariella U.C Sihombing
+import java.util.*;
+import java.lang.Math;
+
+class P02_02 {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String informasiGudang;
+        String namaGudang;
+        int kapasitasGudang;
+        int jumlahBuku;
+        String aC;
+        String lantai;
+        String ketersediaanTeknologi;
+        int skor;
+        String penilaian;
+
+        namaGudang = input.nextLine();
+        kapasitasGudang = input.nextInt();
+        jumlahBuku = input.nextInt();
+        aC = input.nextLine();
+        lantai = input.nextLine();
+        ketersediaanTeknologi = input.nextLine();
+        skor = (int) ((double) jumlahBuku / kapasitasGudang * 100);
+        if (skor >= 40) {
+            penilaian = "Gudang Elite";
+        } else {
+            if (skor >= 25) {
+                penilaian = "Gudang Standar";
+            } else {
+                if (skor < 25) {
+                    penilaian = "Gudang Perlu Peningkatan";
+                }
+            }
+        }
+        System.out.println(namaGudang + "|" + kapasitasGudang + "|" + jumlahBuku + "|" + aC + "|" + lantai + "|" + ketersediaanTeknologi + "|" + skor + "|" + penilaian);
+    }
+}
