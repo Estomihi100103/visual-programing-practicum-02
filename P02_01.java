8bc8b98fcee56d1046d658acb1029b6877108f75
AlyaTriswani <143679147+AlyaTriswani@users.noreply.github.com>
2023-09-30 21:40:12 +0700

    telah menyelesaikan tugas

--- P02_01.java
+++ P02_01.java
@@ -1,1 +1,24 @@
-// NIM - Your Name
+//  12S23038 - Alya Triswani
+
+import java.util.*;
+import java.lang.Math;
+
+class Program {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String namagudang, aC, lantai, ketersediaanteknologi;
+        int kapasitasgudang, jumlahbukusaatini;
+        String informasigudang;
+
+        namagudang = input.nextLine();
+        kapasitasgudang = input.nextInt();
+        jumlahbukusaatini = input.nextInt();
+        aC = input.nextLine();
+        lantai = input.nextLine();
+        ketersediaanteknologi = input.nextLine();
+        System.out.println("Informasi gudang: ");
+        System.out.println(namagudang + " | " + kapasitasgudang + " | " + jumlahbukusaatini + " | " + aC + " | " + lantai + " | " + ketersediaanteknologi);
+    }
+}
+
--- P02_02.java
+++ P02_02.java
@@ -1,1 +1,26 @@
-//NIM -Nama
+//12S23038 -Alya Triswani
+
+Function Main
+    Declare String nama, ac, lantai, teknologi, gudang
+    Declare Integer kapasitas, jumlah, skor, x
+
+    Input nama
+    Input kapasitas
+    Input jumlah
+    Input ac
+    Input lantai
+    Input teknologi
+    Assign skor = (jumlah/kapasitas)*100
+    If skor>=40
+        Assign gudang = "gudang elit"
+    Else
+        If skor<40 and skor>=25
+            Assign gudang = "gudang standar"
+        Else
+            If skor<25
+                Assign gudang = "gudang perlu peningkatan"
+            End
+        End
+    End
+    Output gudang&"|"&kapasitas&"|"&jumlah&"|"&ac&"|"&lantai&"|"&teknologi&"|"&skor&"|"&gudang
+End
