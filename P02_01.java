2dbe08aeb8bdb0a12af078844d33bef54b2712e7
JenniferSihotang <jennifersihotang04@gmail.com>
2023-09-30 21:33:55 +0700

    Sudah menyelesaikan tugas.

--- P02_01.java
+++ P02_01.java
@@ -1,1 +1,25 @@
-// NIM - Your Name
+// 12S23047 - Jennifer Hopenes Sihotang
+
+import java.util.*;
+import java.lang.Math;
+
+class 01 {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String deskripsiTugas;
+        String kodeMataKuliah;
+        String namaMataKuliah;
+        String pengampu;
+        String deadLine;
+        String formatPenamaanTugas;
+
+        deskripsiTugas = input.nextLine();
+        kodeMataKuliah = input.nextLine();
+        namaMataKuliah = input.nextLine();
+        pengampu = input.nextLine();
+        deadLine = input.nextLine();
+        formatPenamaanTugas = input.nextLine();
+        System.out.println(deskripsiTugas + "|" + kodeMataKuliah + "|" + namaMataKuliah + "|" + pengampu + "|" + deadLine + "|" + formatPenamaanTugas);
+    }
+}
