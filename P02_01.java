42189e66e9e1c3bb24b5af915e74902d4aa8c821
GraceTiodora <gracetheodora67@gmail.com>
2023-09-30 21:22:54 +0700

    sudah menyelesaikan tugas

--- P02_01.java
+++ P02_01.java
@@ -1,1 +1,37 @@
-// NIM - Your Name
+// 12S23043 - Grace Tiodora
+import java.util.*;
+import java.lang.Math;
+
+class P02_01 {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String program, code, matkul, dosen, tanggal, format, x;
+        double tingkatKesulitan, hariDeadline;
+        double prioritas;
+        String status;
+
+        program = input.nextLine();
+        code = input.nextLine();
+        matkul = input.nextLine();
+        dosen = input.nextLine();
+        tanggal = input.nextLine();
+        format = input.nextLine();
+        tingkatKesulitan = input.nextDouble();
+        hariDeadline = input.nextDouble();
+        status = input.nextLine();
+        prioritas = tingkatKesulitan * (1.0 / hariDeadline);
+        if (prioritas > 3) {
+            x = "Penting! Anda harus mengerjakan tugas ini segera.";
+        } else {
+            if (prioritas >= 1.5 && prioritas <= 3) {
+                x = "Tugas ini memiliki prioritas menengah.";
+            } else {
+                if (prioritas < 1.5) {
+                    x = "Tugas ini relatif ringan, namun jangan tunda terlalu lama.";
+                }
+            }
+        }
+        System.out.println(program + "|" + code + "|" + matkul + "|" + dosen + "|" + tanggal + "|" + format + "|" + tingkatKesulitan + "|" + hariDeadline + "|" + status + "|" + x);
+    }
+}
--- P02_02.java
+++ P02_02.java
@@ -1,1 +1,41 @@
-//NIM -Nama
+//12S23043 -Grace Tiodora
+import java.util.*;
+import java.lang.Math;
+
+class P02_02 {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String namaGudang, aC, lantai, ketersediaanTeknologi, gedungElit, gedungStandart, gedungPerluPeningkatan, status;
+        int kapasitasGudang, jumlahbuku, skor;
+
+        System.out.println("Nama Gudang : ");
+        namaGudang = input.nextLine();
+        System.out.println("Kapasitas Gudang :");
+        kapasitasGudang = input.nextInt();
+        System.out.println("Jumlah Buku Saat Ini :");
+        jumlahbuku = input.nextInt();
+        System.out.println("AC :");
+        aC = input.nextLine();
+        System.out.println("Lantai :");
+        lantai = input.nextLine();
+        System.out.println("Ketersediaan Teknologi :");
+        ketersediaanTeknologi = input.nextLine();
+        skor = (int) ((double) jumlahbuku / kapasitasGudang * 100);
+        if (skor >= 40) {
+            status = "Gedung Elit";
+            System.out.println("Gedung Ini Termasuk Gudang Elit");
+        } else {
+            if (skor >= 25) {
+                status = "Gedung Standart";
+                System.out.println("Gedung Ini Termasuk Gedung Standart");
+            } else {
+                if (skor < 25) {
+                    status = "Gedung Perlu Peningkatan";
+                    System.out.println("Gedung Ini Termasuk Gedung Perlu Peningkatan");
+                }
+            }
+        }
+        System.out.println(namaGudang + "|" + kapasitasGudang + "|" + jumlahbuku + "|" + aC + "|" + lantai + "|" + ketersediaanTeknologi + "|" + skor + "|" + status);
+    }
+}
