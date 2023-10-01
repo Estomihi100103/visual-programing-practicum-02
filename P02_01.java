711831c21abfe44101ec78a2c5b86bad34492229
AlyaTriswani <143679147+AlyaTriswani@users.noreply.github.com>
2023-10-01 16:01:53 +0700

    P02

--- P02_01.java
+++ P02_01.java
@@ -7,18 +7,37 @@ class Program {
     private static Scanner input = new Scanner(System.in);
 
     public static void main(String[] args) {
-        String namagudang, aC, lantai, ketersediaanteknologi;
-        int kapasitasgudang, jumlahbukusaatini;
-        String informasigudang;
+        String deskripsitugas, kodematakuliah, namamatakuliah, dosenpengampu, deadline, formatpenamaantugas, rekomendasitugas, status;
+        double prioritas;
+        double tingkatkesulitan, harideadline;
 
-        namagudang = input.nextLine();
-        kapasitasgudang = input.nextInt();
-        jumlahbukusaatini = input.nextInt();
-        aC = input.nextLine();
-        lantai = input.nextLine();
-        ketersediaanteknologi = input.nextLine();
-        System.out.println("Informasi gudang: ");
-        System.out.println(namagudang + " | " + kapasitasgudang + " | " + jumlahbukusaatini + " | " + aC + " | " + lantai + " | " + ketersediaanteknologi);
+        deskripsitugas = input.nextLine();
+        kodematakuliah = input.nextLine();
+        namamatakuliah = input.nextLine();
+        dosenpengampu = input.nextLine();
+        deadline = input.nextLine();
+        formatpenamaantugas = input.nextLine();
+        tingkatkesulitan = input.nextDouble();
+        harideadline = input.nextDouble();
+        status = input.nextLine();
+        prioritas = tingkatkesulitan * (1.0 / harideadline);
+        if (prioritas > 3.0) {
+            rekomendasitugas = "Penting!Anda harus mengerjakan tugas ini segera";
+        } else {
+            if (prioritas < 3.0 && prioritas > 1.5) {
+                rekomendasitugas = "Tugas ini memiliki prioritas menengah";
+            } else {
+                if (prioritas < 1.5) {
+                    rekomendasitugas = "Tugas ini relatif ringan, namun jangan tunda terlalu lama.";
+                }
+            }
+        }
+        System.out.println("prioritas:" + toFixed(prioritas,2));
+        System.out.println(deskripsitugas + "|" + kodematakuliah + "|" + namamatakuliah + "|" + dosenpengampu + "|" + deadline + "|" + formatpenamaantugas + "|" + tingkatkesulitan + "|" + harideadline + "|" + status + "|" + rekomendasitugas);
+    }
+    
+    private static String toFixed(double value, int digits) {
+        return String.format("%." + digits + "f", value);
     }
 }
 
--- P02_02.java
+++ P02_02.java
@@ -1,26 +1,33 @@
 //12S23038 -Alya Triswani
 
-Function Main
-    Declare String nama, ac, lantai, teknologi, gudang
-    Declare Integer kapasitas, jumlah, skor, x
+import java.util.*;
+import java.lang.Math;
 
-    Input nama
-    Input kapasitas
-    Input jumlah
-    Input ac
-    Input lantai
-    Input teknologi
-    Assign skor = (jumlah/kapasitas)*100
-    If skor>=40
-        Assign gudang = "gudang elit"
-    Else
-        If skor<40 and skor>=25
-            Assign gudang = "gudang standar"
-        Else
-            If skor<25
-                Assign gudang = "gudang perlu peningkatan"
-            End
-        End
-    End
-    Output gudang&"|"&kapasitas&"|"&jumlah&"|"&ac&"|"&lantai&"|"&teknologi&"|"&skor&"|"&gudang
-End
+class Program {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String nama, ac, lantai, teknologi, gudang;
+        int kapasitas, jumlah, skor, x;
+
+        nama = input.nextLine();
+        kapasitas = input.nextInt();
+        jumlah = input.nextInt();
+        ac = input.nextLine();
+        lantai = input.nextLine();
+        teknologi = input.nextLine();
+        skor = (int) ((double) jumlah / kapasitas * 100);
+        if (skor >= 40) {
+            gudang = "gudang elit";
+        } else {
+            if (skor < 40 && skor >= 25) {
+                gudang = "gudang standar";
+            } else {
+                if (skor < 25) {
+                    gudang = "gudang perlu peningkatan";
+                }
+            }
+        }
+        System.out.println(gudang + "|" + kapasitas + "|" + jumlah + "|" + ac + "|" + lantai + "|" + teknologi + "|" + skor + "|" + gudang);
+    }
+}
