abdde4450c47808df68243c030641cbf21641842
Danielsitumorang1 <143679668+Danielsitumorang1@users.noreply.github.com>
2023-10-01 14:47:32 +0700

    P02
    Sudah menyelesaikan tugas

--- P02_01.java
+++ P02_01.java
@@ -1,1 +1,42 @@
-// NIM - Your Name
+// 12S23028 - Daniel Situmorang
+import java.util.*;
+import java.lang.Math;
+
+class P02_01 {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String program, kode, matkul, dosen, tanggal, format, hasil;
+        double tingkatkesulitan, deadline, prioritas;
+        String status;
+
+        program = input.nextLine();
+        kode = input.nextLine();
+        matkul = input.nextLine();
+        dosen = input.nextLine();
+        tanggal = input.nextLine();
+        format = input.nextLine();
+        tingkatkesulitan = input.nextDouble();
+        deadline = input.nextDouble();
+        status = input.nextLine();
+        prioritas = tingkatkesulitan * (1.0 / deadline);
+        if (prioritas > 3.0) {
+            hasil = "Penting! Anda harus mengerjakan tugas ini segera.";
+        } else {
+            if (prioritas >= 1.5 && prioritas <= 3.0) {
+                hasil = "Tugas ini memiliki prioritas menengah.";
+            } else {
+                if (prioritas < 1.5) {
+                    hasil = "Tugas ini relatif ringan, namun jangan tunda terlalu lama.";
+                }
+            }
+        }
+        System.out.println("prioritas : " + toFixed(prioritas,2));
+        System.out.println(program + "|" + kode + "|" + matkul + "|" + dosen + "|" + tanggal + "|" + format + "|" + status + "|" + hasil);
+    }
+    
+    private static String toFixed(double value, int digits) {
+        return String.format("%." + digits + "f", value);
+    }
+}
+
--- P02_02.java
+++ P02_02.java
@@ -1,1 +1,32 @@
-//NIM -Nama
+// 12S23028 - Daniel Situmorang
+import java.util.*;
+import java.lang.Math;
+
+class P02_02 {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String namagudang, aC, lantai, ketersediaan, kualitas;
+        double kapasitas, jumlah, skor;
+
+        namagudang = input.nextLine();
+        kapasitas = input.nextDouble();
+        jumlah = input.nextDouble();
+        aC = input.nextLine();
+        lantai = input.nextLine();
+        ketersediaan = input.nextLine();
+        skor = jumlah / kapasitas * 100;
+        if (skor >= 40) {
+            kualitas = "Gudang Elite";
+        } else {
+            if (skor >= 25) {
+                kualitas = "Gudang Standar";
+            } else {
+                if (skor < 25) {
+                    kualitas = "Gudang Perlu Peningkatan";
+                }
+            }
+        }
+        System.out.println(namagudang + "|" + kapasitas + "|" + jumlah + "|" + aC + "|" + lantai + "|" + ketersediaan + "|" + skor + "|" + kualitas);
+    }
+}
