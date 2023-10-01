d7f322f4329ec2a55909d0e5cf1c58f420cde28a
JenniferSihotang <jennifersihotang04@gmail.com>
2023-10-01 13:29:23 +0700

    Update P02_01.java

--- P02_01.java
+++ P02_01.java
@@ -7,19 +7,36 @@ class 01 {
     private static Scanner input = new Scanner(System.in);
 
     public static void main(String[] args) {
-        String deskripsiTugas;
-        String kodeMataKuliah;
-        String namaMataKuliah;
-        String pengampu;
-        String deadLine;
-        String formatPenamaanTugas;
+        String namaTugas, kode, matkul, namaDosen, deadline, format, status, hasil1, hasil2, hasil3;
+        double prioritas;
+        int hari, tingkatKesulitan;
 
-        deskripsiTugas = input.nextLine();
-        kodeMataKuliah = input.nextLine();
-        namaMataKuliah = input.nextLine();
-        pengampu = input.nextLine();
-        deadLine = input.nextLine();
-        formatPenamaanTugas = input.nextLine();
-        System.out.println(deskripsiTugas + "|" + kodeMataKuliah + "|" + namaMataKuliah + "|" + pengampu + "|" + deadLine + "|" + formatPenamaanTugas);
+        namaTugas = input.nextLine();
+        kode = input.nextLine();
+        matkul = input.nextLine();
+        namaDosen = input.nextLine();
+        deadline = input.nextLine();
+        format = input.nextLine();
+        tingkatKesulitan = input.nextInt();
+        hari = input.nextInt();
+        status = input.nextLine();
+        prioritas = tingkatKesulitan * (1.0 / hari);
+        System.out.println(" prioritas :" + toFixed(prioritas,2));
+        if (prioritas > 3) {
+            hasil1 = "Penting! Anda harus mengerjakan tugas ini segera.";
+        } else {
+            if (prioritas <= 3 && prioritas >= 1.5) {
+                hasil1 = "Tugas ini memiliki prioritas menengah.";
+            } else {
+                if (prioritas <= 1.5) {
+                    hasil1 = "Tugas ini relatif ringan, namun jangan tunda terlalu lama.";
+                }
+            }
+        }
+        System.out.println(namaTugas + "|" + kode + "|" + matkul + "|" + namaDosen + "|" + deadline + "|" + format + "|" + status + "|" + hasil1);
+    }
+    
+    private static String toFixed(double value, int digits) {
+        return String.format("%." + digits + "f", value);
     }
 }
