00448ef0b721a439c88ae20c9aec3336e44f141a
YolandaSSaragih <143678937+YolandaSSaragih@users.noreply.github.com>
2023-10-01 14:29:42 +0700

    12S23050_Yolanda Septania Saragih

--- P02_02.java
+++ P02_02.java
@@ -1,1 +1,34 @@
-//NIM -Nama
+// 12S23050 -Yolanda Septania Saragih
+
+import java.util.*;
+import java.lang.Math;
+
+class  P02_02 {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String namagudang, aC, lantai, ketersediaanteknologi;
+        int kapasitas, jumlahbukusaatini, skor, kualitas;
+
+        namagudang = input.nextLine();
+        kapasitas = input.nextInt();
+        jumlahbukusaatini = input.nextInt();
+        aC = input.nextLine();
+        lantai = input.nextLine();
+        ketersediaanteknologi = input.nextLine();
+        skor = (int) ((double) jumlahbukusaatini / kapasitas * 100);
+        if (skor >= 40) {
+            kualitas = "gudang elite";
+        } else {
+            if (skor >= 25) {
+                kualitas = "gudang standar";
+            } else {
+                if (skor >= 20) {
+                    kualitas = "gudang perlu peningkatan";
+                }
+            }
+        }
+        System.out.println("informasi gudang");
+        System.out.println(namagudang + "|" + kapasitas + "|" + jumlahbukusaatini + "|" + aC + "|" + lantai + "|" + ketersediaanteknologi);
+    }
+}
