e28d35f20544e5eea39a1fb1b947bbd720757b6b
SimorangkirJonathan <143678943+SimorangkirJonathan@users.noreply.github.com>
2023-10-01 13:43:06 +0700

    12S23030-Simorangkir Jonathan

--- P02_02.java
+++ P02_02.java
@@ -1,1 +1,34 @@
-//NIM -Nama
+//12S23030_Simorangkir Jonathan 
+
+import java.util.*;
+import java.lang.Math;
+
+class P02-02 {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String namagudang, aC, lantai, ketersediaanteknologi, kapasitas1;
+        int jumlah, kapasitas;
+        String informasigudang;
+
+        namagudang = input.nextLine();
+        kapasitas = input.nextInt();
+        jumlah = input.nextInt();
+        aC = input.nextLine();
+        lantai = input.nextLine();
+        ketersediaanteknologi = input.nextLine();
+        kapasitas = (int) ((double) jumlah / kapasitas * 100);
+        if (kapasitas > 40) {
+            kapasitas1 = "gudang elite";
+        } else {
+            if (kapasitas < 40 && kapasitas > 25) {
+                kapasitas1 = "gudang standar";
+            } else {
+                if (kapasitas < 25) {
+                    kapasitas1 = "gudang aneh";
+                }
+            }
+        }
+        System.out.println(namagudang + "|" + kapasitas + "|" + jumlah + "|" + aC + "|" + lantai + "|" + ketersediaanteknologi + "|" + kapasitas1);
+    }
+}
