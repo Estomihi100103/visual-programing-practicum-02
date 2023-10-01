4a1f92bd072be8185739fda3178f93315ab7163d
AnastasyaTabitaBlessingSiahaan <143679126+AnastasyaTabitaBlessingSiahaan@users.noreply.github.com>
2023-10-01 14:04:32 +0700

    12S23046_Anastaya Tabita Blessing Siahaan

--- P02_01.java
+++ P02_01.java
@@ -1,38 +1,41 @@
-// NIM - Your Name
-main()
-{
-    // Deklarasi variabel
-    int a, b, c;
-    // Input
-    System.out.print("Masukkan nilai a: ");
-    a = baca.nextInt();
-    System.out.print("Masukkan nilai b: ");
-    b = baca.nextInt();
-    System.out.print("Masukkan nilai c: ");
-    c = baca.nextInt();
-    // Proses
-    if (a > b)
-    {
-        if (a > c)
-        {
-            System.out.println("Nilai a terbesar");
+// 12S23046-Anastasya Tabita Blessing Siahaan
+
+import java.util.*;
+import java.lang.Math;
+
+class P02_01 {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String program, kode, matkul, dosen, tanggal, format, status, rekomendasitugas;
+        double tingkatkesulitan, harideadline, prioritas;
+
+        program = input.nextLine();
+        kode = input.nextLine();
+        matkul = input.nextLine();
+        dosen = input.nextLine();
+        tanggal = input.nextLine();
+        format = input.nextLine();
+        tingkatkesulitan = input.nextDouble();
+        harideadline = input.nextDouble();
+        status = input.nextLine();
+        prioritas = tingkatkesulitan * (1.0 / harideadline);
+        System.out.println(" prioritas: " + toFixed(prioritas,2));
+        if (prioritas > 3.0) {
+            rekomendasitugas = "penting! anda harus mengerjakan tugas ini segera";
+        } else {
+            if (prioritas >= 1.5 && prioritas > 3) {
+                rekomendasitugas = "tugas ini memiliki prioritas menengah";
+            } else {
+                if (prioritas < 1.5) {
+                    rekomendasitugas = "tugas ini relatif ringan, namun jangan tunda terlalu lama";
+                }
+            }
         }
-        else
-        {
-            System.out.println("Nilai c terbesar");
-        }
+        System.out.println(program + "|" + kode + "|" + matkul + "|" + dosen + "|" + tanggal + "|" + format + "|" + status + "|" + prioritas + "|" + rekomendasitugas);
     }
-    else
-    {
-        if (b > c)
-        {
-            System.out.println("Nilai b terbesar");
-        }
-        else
-        {
-            System.out.println("Nilai c terbesar");
-        }
+    
+    private static String toFixed(double value, int digits) {
+        return String.format("%." + digits + "f", value);
     }
-    // Output
 }
-```
