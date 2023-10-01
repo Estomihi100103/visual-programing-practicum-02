239d2ecb59768683462da8b923f4e1a71a3db861
Eunikepurba <eunikepurba50@gmail.com>
2023-10-01 14:53:54 +0700

    12S23021 - Eunike Purba

--- P02_01.java
+++ P02_01.java
@@ -1,1 +1,42 @@
-// NIM - Your Name
+// 12S23021 - Eunike Purba
+import java.util.*;
+import java.lang.Math;
+
+class P02_01 {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String nAMATUGAS, mATAKULIAH, dOSENPENGAMPU, dEADLINE, nIMNAMATUGAS, sTATUS, rEKOMENDASITUGAS, kODEMATKUL;
+        int tINGKATKESULITAN, hARIHINGGADEADLINE;
+        double pRIORITAS;
+
+        nAMATUGAS = input.nextLine();
+        kODEMATKUL = input.nextLine();
+        mATAKULIAH = input.nextLine();
+        dOSENPENGAMPU = input.nextLine();
+        dEADLINE = input.nextLine();
+        nIMNAMATUGAS = input.nextLine();
+        tINGKATKESULITAN = input.nextInt();
+        hARIHINGGADEADLINE = input.nextInt();
+        sTATUS = input.nextLine();
+        pRIORITAS = tINGKATKESULITAN * (1.0 / hARIHINGGADEADLINE);
+        if (pRIORITAS > 3.0) {
+            rEKOMENDASITUGAS = "PENTING,HARUS MENGERJAKANNYA SEGERA";
+        } else {
+            if (pRIORITAS < 3.0 && pRIORITAS > 1.5) {
+                rEKOMENDASITUGAS = "TUGAS INI MEMILIKI PRIORITAS MENENGAH";
+            } else {
+                if (pRIORITAS < 1.5) {
+                    rEKOMENDASITUGAS = "TUGAS RELATIF RINGAN, JANGAN MENUNDA TERLALU LAMA";
+                }
+            }
+        }
+        System.out.println("PRIORITAS:" + toFixed(pRIORITAS,2));
+        System.out.println(nAMATUGAS + "|" + kODEMATKUL + "|" + mATAKULIAH + "|" + dOSENPENGAMPU + "|" + dEADLINE + "|" + nIMNAMATUGAS + "|" + sTATUS + "|" + rEKOMENDASITUGAS);
+    }
+    
+    private static String toFixed(double value, int digits) {
+        return String.format("%." + digits + "f", value);
+    }
+}
+
--- P02_02.java
+++ P02_02.java
@@ -1,1 +1,35 @@
-//NIM -Nama
+// 12S23021 - Eunike Purba
+import java.util.*;
+import java.lang.Math;
+
+class P02_02 {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String nama, ac, lantai, ketersediaanteknologi, kualitasgudang, nilai;
+        int kapasitas, jumlahbuku, skor;
+
+        nama = input.nextLine();
+        ac = input.nextLine();
+        lantai = input.nextLine();
+        ketersediaanteknologi = input.nextLine();
+        kualitasgudang = input.nextLine();
+        kapasitas = input.nextInt();
+        jumlahbuku = input.nextInt();
+        skor = (int) ((double) jumlahbuku / kapasitas * 100);
+        if (skor >= 40) {
+            nilai = "gudang elit";
+        } else {
+            if (skor >= 25 && nilai <= 40) {
+                nilai = "gudangstandar";
+            } else {
+                if (skor <= 25) {
+                    nilai = "gudang perlu peningkatan";
+                } else {
+                    nilai = "gudangperlupeningkatan";
+                }
+            }
+        }
+        System.out.println(nama + "|" + kapasitas + "|" + jumlahbuku + "|" + ac + "|" + lantai + "|" + ketersediaanteknologi + "|" + skor + "|" + nilai);
+    }
+}
