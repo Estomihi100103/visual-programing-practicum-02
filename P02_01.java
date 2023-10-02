d9123ee8154bee7d44278cb02fcf4459e3163e96
ArifDoloksaribu <143679155+ArifDoloksaribu@users.noreply.github.com>
2023-10-01 23:39:57 -0700

    P02
    Telah menyelesaikan tugas tutor

--- P02_01.java
+++ P02_01.java
@@ -1,1 +1,41 @@
-// NIM - Your Name
+// 12S23026_Arif M. Doloksaribu
+import java.util.*;
+import java.lang.Math;
+
+class P02_01 {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String program, kode, matkul, dosen, tanggal, format, kinerja;
+        double tingkatkesulitan, harideadline, prioritas;
+        String status;
+
+        program = input.nextLine();
+        kode = input.nextLine();
+        matkul = input.nextLine();
+        dosen = input.nextLine();
+        tanggal = input.nextLine();
+        format = input.nextLine();
+        tingkatkesulitan = input.nextDouble();
+        harideadline = input.nextDouble();
+        status = input.nextLine();
+        prioritas = tingkatkesulitan * (1.0 / harideadline);
+        if (prioritas > 3) {
+            kinerja = "penting! harus segera dikerjakan";
+        } else {
+            if (prioritas <= 3 && prioritas >= 1.5) {
+                kinerja = "tugas ini memiliki prioritas menengah";
+            } else {
+                if (prioritas < 1.5) {
+                    kinerja = "tugas ini relatif ringan, namun jangan tunda terlalu lama";
+                }
+            }
+        }
+        System.out.println("prioritas : " + toFixed(prioritas,2));
+        System.out.println(program + "|" + kode + "|" + matkul + "|" + dosen + "|" + tanggal + "|" + format + "|" + kinerja + "|" + tingkatkesulitan + "|" + harideadline + "|" + status);
+    }
+    
+    private static String toFixed(double value, int digits) {
+        return String.format("%." + digits + "f", value);
+    }
+}
--- P02_02.java
+++ P02_02.java
@@ -1,1 +1,33 @@
-//NIM -Nama
+// 12S23026_Arif M. Doloksaribu
+import java.util.*;
+import java.lang.Math;
+
+class P02_02 {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String namagudang, aC, lantai, ketersediaanteknologi, kualitasgudang;
+        int kapasitasgudang, jumlahbukusaatini, poinGudang;
+
+        namagudang = input.nextLine();
+        kapasitasgudang = input.nextInt();
+        jumlahbukusaatini = input.nextInt();
+        aC = input.nextLine();
+        lantai = input.nextLine();
+        ketersediaanteknologi = input.nextLine();
+        poinGudang = (int) ((double) jumlahbukusaatini / kapasitasgudang * 100);
+        if (poinGudang >= 40) {
+            kualitasgudang = "Gudang Elite";
+        } else {
+            if (poinGudang >= 25 && poinGudang < 40) {
+                kualitasgudang = "Gudang Standar";
+            } else {
+                if (poinGudang < 25) {
+                    kualitasgudang = "Gudang Perlu Peningkatan";
+                }
+            }
+        }
+        System.out.println("Informasi Gudang : ");
+        System.out.println(namagudang + "|" + kapasitasgudang + "|" + jumlahbukusaatini + "|" + aC + "|" + lantai + "|" + ketersediaanteknologi + "|" + poinGudang + "|" + kualitasgudang);
+    }
+}
